16c160b5d67726c9b95c8d246d79b2ed6f2b5c6f
R <ruiweilai@163.com>
2018-02-24 13:14:24 +0800

    finish layout and routes

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -1,10 +1,17 @@
 <!DOCTYPE html>
 <html>
   <head>
-    <meta charset="utf-8">
-    <title>@yield('title', 'Sample App') - Laravel 新手入门教程</title>
+    <title>@yield('title', 'Sample App') - Laravel 入门教程</title>
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
   </head>
   <body>
-      @yield('content')
+    @include('layouts._header')
+
+    <div class="container">
+      <div class="col-md-offset-1 col-md-10">
+        @yield('content')
+        @include('layouts._footer')
+      </div>
+    </div>
   </body>
-</html>
+</html>>
